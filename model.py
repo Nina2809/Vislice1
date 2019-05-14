b6ccea5e5ab1ffd039227b13e40d57e1f75ef0e5
Nina Velkavrh <nina.velkavrh@student.fmf.uni-lj.si>
2019-05-14 19:29:02 +0200

    spremebe pri metodah

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-ŠTEVILO_DOVOLJENIH_NAPAK = 10
+STEVILO_DOVOLJENIH_NAPAK = 10
 
 PRAVILNA_CRKA = '+'
 PONOVLJENA_CRKA = 'o'
@@ -13,50 +13,50 @@ class Igra:
         self.crke = crke
 
 
-def pravilne_crke(self):
-    return [crka for crka in self.crke if crka in self.geslo]    
- 
-def napacne_crke(self):
-    return [crka for crka in self.crke if crka not in self.geslo]
+    def pravilne_crke(self):
+        return [crka for crka in self.crke if crka in self.geslo]    
+    
+    def napacne_crke(self):
+        return [crka for crka in self.crke if crka not in self.geslo]
 
-def stevilo_napak(self):
-    return len(self.napacne_crke)
+    def stevilo_napak(self):
+        return len(self.napacne_crke())
 
-def zmaga(self):
-    for crka in self.geslo:
-        if crka in self.crke:
-            return False
-    return True
+    def zmaga(self):
+        for crka in self.geslo:
+            if crka in self.crke:
+                return False
+        return True
 
-def poraz(self):
-    return self.stevilo_napak() > STEVILO_DOVOLJENIH_NAPAK
+    def poraz(self):
+        return self.stevilo_napak() > STEVILO_DOVOLJENIH_NAPAK
 
-def pravilni_del_gesla(self):
-    izpis = ''
-    for crka in self.geslo:
-        if crka in self.pravilne_crke:
-            izpis += crka
-        else:
-            izpis += '_'
-    return izpis
+    def pravilni_del_gesla(self):
+        izpis = ''
+        for crka in self.geslo:
+            if crka in self.pravilne_crke():
+                izpis += crka
+            else:
+                izpis += '_'
+        return izpis
 
-def nepravilni_ugibi(self):
-    return ' '.join(self.napacne_crke())
+    def nepravilni_ugibi(self):
+        return ' '.join(self.napacne_crke())
 
-def ugibaj(self, crka):
-    velika_crka = crka.upper()
-    if velika_crka in self.crke:
-        return PONOVLJENA_CRKA
-    self.crke.append(velika_crka)
-    if self.zmaga():
-        return ZMAGA
-    elif self.poraz():
-        return PORAZ
-    else: 
-        if velika_crka in self.pravilne_crke():
-            return PRAVILNA_CRKA
-        elif velika_crka in self.napacne_crke():
-                return NAPACNA_CRKA
+    def ugibaj(self, crka):
+        velika_crka = crka.upper()
+        if velika_crka in self.crke:
+            return PONOVLJENA_CRKA
+        self.crke.append(velika_crka)
+        if self.zmaga():
+            return ZMAGA
+        elif self.poraz():
+            return PORAZ
+        else: 
+            if velika_crka in self.pravilne_crke():
+                return PRAVILNA_CRKA
+            elif velika_crka in self.napacne_crke():
+                    return NAPACNA_CRKA
 
 bazen_besed = []
 with open('besede.txt') as f:
--- tekstovni_vmesnik.py
+++ tekstovni_vmesnik.py
@@ -6,14 +6,12 @@ def izpis_igre(igra):
 Napačne črke: {napacne_crke}
 ugibaš še {stevilo}.krat.
 =================================================""".format(
-    geslo=igra.pravilni_del_gesla(),
-    napacne_crke=igra.nepravilni_ugibi(),
-    stevilo=model.STEVILO_DOVOLJENIH_NAPAK - igra.stevilo
-
-)
+    geslo = igra.pravilni_del_gesla(),
+    napacne_crke = igra.nepravilni_ugibi(),
+    stevilo = model.STEVILO_DOVOLJENIH_NAPAK - igra.stevilo_napak())
 
 def izpis_zmage(igra):
-    return "Čestitke! uganil/a si geslo {}.".format(igra)
+    return "Čestitke! uganil/a si geslo {}.".format(igra.geslo)
 
 def izpis_poraza(igra):
     return "Več sreče prihodnič!"
